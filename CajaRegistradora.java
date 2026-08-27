1f2d77ef493101d6481b3a277ccf48b02d65dbd6
reinosodeily <deilyyulia12@gmail.com>
2026-08-27 17:48:02 -0500

    realice cambios de variables y reduje lineas

--- CajaRegistradora.java
+++ CajaRegistradora.java
@@ -6,35 +6,41 @@ public class CajaRegistradora {
         Scanner leer = new Scanner(System.in);
 
         String articulo;
-        Double valorAticulo;
+        double valorAticulo  ;
         boolean categoria = true;
+        double acomuladorValorArticulo = 0.0;
+       double descuento= 0.10;
+       double IMPUESTO_IVA = 0.10;
+       int contadorPemiun = 0;
+
 
         do{
- System.out.println("ingrese el nombre del articulo");
-        articulo =leer.next();
-
-        System.out.println("ingrese el valor del producto");
-        valorAticulo = leer.nextDouble();
-
-        System.out.println("elija el tipo de categoria del producto");
+        System.out.println(" seleccione el tipo de producto del 1 al 4");
         System.out.println("1.basico");
         System.out.println("2.medio");
         System.out.println("3.premiun");
         System.out.println("4.finalizar compra");
-
          articulo = leer.next();
+
+ System.out.println("ingrese el valor del producto");
+        valorAticulo = leer.nextDouble();
 
         switch (articulo) {
             case "1":
                  System.out.println("ha elegido el articulo basico");
+                 acomuladorValorArticulo += valorAticulo;
                 break;
 
                 case "2":
                  System.out.println("ha elegido el articulo medio");
+                 acomuladorValorArticulo += valorAticulo;
                 break;
 
                 case "3":
                  System.out.println("ha elegido el articulo premiun");
+                 acomuladorValorArticulo += valorAticulo;
+                 contadorPemiun ++;
+                 
                 break;
 
         case "4":
@@ -42,13 +48,18 @@ public class CajaRegistradora {
                  categoria = false;
                 break;
 
-            default:
-
-            System.out.println("opcion incorrecta");
-                break;
         }
         } while (categoria);
+       
         
+            if (acomuladorValorArticulo > 100000) {
+                acomuladorValorArticulo = acomuladorValorArticulo- (acomuladorValorArticulo*descuento);
+                System.out.println( "el valor de la cuenta co el descuento es" + acomuladorValorArticulo);
+
+                
+            }else {System.out.println("pagar el valor total"+acomuladorValorArticulo);
+
+            }
             
         
     }
