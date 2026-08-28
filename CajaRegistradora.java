6577da945fe3bdf30c0a6baca6547fa0fa6745b3
reinosodeily <deilyyulia12@gmail.com>
2026-08-27 20:41:03 -0500

    se agregan variables y lineas al codigo

--- CajaRegistradora.java
+++ CajaRegistradora.java
@@ -5,62 +5,77 @@ public class CajaRegistradora {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
 
-        String articulo;
+         String articulo;
         double valorAticulo  ;
         boolean categoria = true;
-        double acomuladorValorArticulo = 0.0;
-       double descuento= 0.10;
-       double IMPUESTO_IVA = 0.10;
-       int contadorPemiun = 0;
+        double acomuladorTotalArticulo  = 0.0;
+        double descuento= 0.10;
+        double IMPUESTO_IVA = 0.10;
+        int contadorPremiun = 0;
+       int contadorAticulo = 0;
 
 
-        do{
-        System.out.println(" seleccione el tipo de producto del 1 al 4");
+        do{ System.out.println("ingrese el valor del producto");
+        valorAticulo = leer.nextDouble();
+
+System.out.println(" seleccione el tipo de producto del 1 al 4");
         System.out.println("1.basico");
         System.out.println("2.medio");
         System.out.println("3.premiun");
         System.out.println("4.finalizar compra");
          articulo = leer.next();
 
- System.out.println("ingrese el valor del producto");
-        valorAticulo = leer.nextDouble();
-
+     
         switch (articulo) {
             case "1":
                  System.out.println("ha elegido el articulo basico");
-                 acomuladorValorArticulo += valorAticulo;
+                 acomuladorTotalArticulo  += valorAticulo;
+                 contadorAticulo++;
                 break;
 
                 case "2":
                  System.out.println("ha elegido el articulo medio");
-                 acomuladorValorArticulo += valorAticulo;
+                 acomuladorTotalArticulo  += valorAticulo;
+                 contadorAticulo++;
                 break;
 
                 case "3":
                  System.out.println("ha elegido el articulo premiun");
-                 acomuladorValorArticulo += valorAticulo;
-                 contadorPemiun ++;
+                 acomuladorTotalArticulo += valorAticulo;
+                 contadorPremiun ++; contadorAticulo++;
                  
                 break;
 
-        case "4":
+                case "4":
                  System.out.println("compra finalizada");
                  categoria = false;
                 break;
 
+                default:
+                    System.out.println("Opción invalidad");
+                    break;
         }
         } while (categoria);
        
         
-            if (acomuladorValorArticulo > 100000) {
-                acomuladorValorArticulo = acomuladorValorArticulo- (acomuladorValorArticulo*descuento);
-                System.out.println( "el valor de la cuenta co el descuento es" + acomuladorValorArticulo);
-
+            if (acomuladorTotalArticulo > 100000) {
+                acomuladorTotalArticulo = acomuladorTotalArticulo- (acomuladorTotalArticulo*descuento);
+                System.out.println("aplica para descuento");
                 
-            }else {System.out.println("pagar el valor total"+acomuladorValorArticulo);
+            }else {System.out.println("pagar el valor total"+acomuladorTotalArticulo);
 
             }
-            
+             double impuestoIva, totalFinal, porcentajePremiun;;
+             
+             impuestoIva = acomuladorTotalArticulo*IMPUESTO_IVA; 
+             System.out.println("el valor del impueto del IVA es:"+impuestoIva);
+             totalFinal =  acomuladorTotalArticulo + impuestoIva;
+             System.out.println("el valor final:"+totalFinal);
+             porcentajePremiun = (contadorPremiun*100.0) / contadorAticulo;
+             System.out.println("el porcentaje de productos premiun:"+porcentajePremiun);
+
+
+
         
     }
 }
